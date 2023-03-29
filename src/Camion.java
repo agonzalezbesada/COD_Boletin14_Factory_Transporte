8355822ca22034dbb88373b89b032ad6f3ae636c
agonzalezbesada <agonzalezbesada@danielcastelao.org>
2023-03-29 13:35:30 +0200

    Falta Palet

--- src/Camion.java
+++ src/Camion.java
@@ -1,35 +1,31 @@
 public class Camion implements Transporte{
-
+    @Override
     public Float costeTotal(Integer cp) {
         Float coste = 0f;
         return coste;
     }
-
+    @Override
     public Integer tipoEmbalaje(Float x, Float y, Float z, Float peso) {
 
         Integer tipoEmbalaje = 0;
 
         Float dimensiones = x + y + z;
 
-        if (dimensiones>=8) {
+        if (dimensiones<=8 && peso<=5) {
+
+            tipoEmbalaje = ENVOLTORIO_CARTON;
+            return tipoEmbalaje;
+
+        } else if (dimensiones<=8 && peso>5) {
 
             tipoEmbalaje = CAJA_MADERA;
             return tipoEmbalaje;
 
-        } else {
+        } else if (dimensiones>8 && peso>5) {
 
-            if (peso<=5) {
-
-                tipoEmbalaje = ENVOLTORIO_CARTON;
-                return tipoEmbalaje;
-
-            } else {
-
-                tipoEmbalaje = CAJA_MADERA;
-                return tipoEmbalaje;
-
-            }
+            tipoEmbalaje = PALET;
+            return tipoEmbalaje;
         }
-
+        return null;
     }
 }
